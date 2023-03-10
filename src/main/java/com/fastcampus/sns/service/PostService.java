c18ab30f2f6b0a572d708607bc3dd258179b7cd5
Joe <nastillmatic@gmail.com>
2023-03-10 17:15:03 +0900

    part - 8 - 포스트작성 기능 개발하기

--- src/main/java/com/fastcampus/sns/service/PostService.java
+++ src/main/java/com/fastcampus/sns/service/PostService.java
@@ -17,18 +17,10 @@ public class PostService {
     private final PostEntityRepository postEntityRepository;
     private final UserEntityRepository userEntityRepository;
 
-
     @Transactional
     public void create(String title, String body, String userName) {
-
-        // user find
         UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
-
-        // post save
-        postEntityRepository.save(new PostEntity());
-
-        // retrun
-
+        postEntityRepository.save(PostEntity.of(title, body, userEntity));
     }
 }
